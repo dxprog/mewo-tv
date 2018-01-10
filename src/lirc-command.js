c007ae96355efd03fb49ed441742312ea272bec8
Matt Hackmann <matt@dxprog.com>
2018-01-09 20:27:26 -0800

    lirc is not a real command, but irsend is

--- src/lirc-command.js
+++ src/lirc-command.js
@@ -6,7 +6,7 @@ const KEY_PRESS_SLEEP = 500;
 module.exports = function lircCommand(deviceName, keyPresses) {
   keyPresses.forEach(keyName => {
     console.log(`Sending IR command: ${deviceName} ${keyName}`);
-    execSync(`lirc send_once ${deviceName} ${keyName}`);
+    execSync(`irsend send_once ${deviceName} ${keyName}`);
     msleep(KEY_PRESS_SLEEP);
   });
 };
