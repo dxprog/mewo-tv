8940f2bb7d2b0d8af889b62faa0932816508c328
Matt Hackmann <matt@dxprog.com>
2018-01-09 20:39:23 -0800

    sleep duration can be specified for each key

--- src/lirc-command.js
+++ src/lirc-command.js
@@ -1,12 +1,15 @@
 const { execSync } = require('child_process');
 const { msleep } = require('sleep');
 
-const KEY_PRESS_SLEEP = 500;
+const DEFAULT_KEY_PRESS_SLEEP = 500;
 
 module.exports = function lircCommand(deviceName, keyPresses) {
   keyPresses.forEach(keyName => {
-    console.log(`Sending IR command: ${deviceName} ${keyName}`);
+    // See if there's a custom sleep duration
+    let sleepDuration = keyName.split(':');
+    sleepDuration = sleepDuration.length > 1 ? parseInt(sleepDuration[1]) : DEFAULT_KEY_PRESS_SLEEP;
+    console.log(`Sending IR command: ${deviceName} ${keyName}, sleeping ${sleepDuration}`);
     execSync(`irsend send_once ${deviceName} ${keyName}`);
-    msleep(KEY_PRESS_SLEEP);
+    msleep(sleepDuration);
   });
 };
