126da21d9778e3217c14d3e7c2f4c7b060cdad0e
Matt Hackmann <matt@dxprog.com>
2018-01-09 20:27:43 -0800

    mewo can be configured from config.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const responder = require('./src/responder');
 const TvCommandDevice = require('./src/tv-command-device');
 const config = require('./config');
 
-responder().then(responder => {
+responder(config.mewo || {}).then(responder => {
   const lircDeviceName = config['lirc-device'];
   config.commands.forEach(command => {
     console.log(`Creating device ${command.name}`);
